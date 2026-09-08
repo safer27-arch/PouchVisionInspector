c4d44b7774eae8a7a8a48951e9fb6cd9d3bf691c
safer27-arch <safer27@gmail.com>
2026-09-08 22:16:36 +0200

    Add files via upload

--- app/src/main/java/com/pouchvision/inspector/TabActivity.kt
+++ app/src/main/java/com/pouchvision/inspector/TabActivity.kt
@@ -1896,8 +1896,25 @@ NG 후보 영역 : %d개
          *
          * lastBitmap 원본은 변경하지 않습니다.
          */
+        /*
+         * 표시 방식만 공통 Renderer로 변경합니다.
+         *
+         * - TAB 판정 / 후보 검출 알고리즘은 그대로 유지
+         * - "NG 후보 N" 라벨은 ROI 바깥으로 이동
+         * - 빨간 원 선 굵기는 기존의 약 50%
+         */
+        val displayBitmap =
+            MarkerDisplayRenderer.renderGeneric(
+                sourceBitmap = source,
+                roiLeft = roiStartX,
+                roiTop = roiStartY,
+                roiWidth = roi.width,
+                roiHeight = roi.height,
+                regions = markerResult.regions
+            )
+
         lastResultBitmap =
-            markerResult.bitmap
+            displayBitmap
 
         hasInspectionResult =
             true
@@ -1905,7 +1922,7 @@ NG 후보 영역 : %d개
         runOnUiThread {
 
             binding.tabImagePreview.setImageBitmap(
-                markerResult.bitmap
+                displayBitmap
             )
 
             binding.tabImagePreview.imageMatrix =

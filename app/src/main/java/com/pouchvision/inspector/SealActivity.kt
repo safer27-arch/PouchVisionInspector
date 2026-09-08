30644ff342cc6588b07b8aaa36d0230d1a0dd969
safer27-arch <safer27@gmail.com>
2026-09-08 23:23:20 +0200

    Add files via upload

--- app/src/main/java/com/pouchvision/inspector/SealActivity.kt
+++ app/src/main/java/com/pouchvision/inspector/SealActivity.kt
@@ -311,6 +311,22 @@ class SealActivity : AppCompatActivity() {
             }
 
         /*
+         * SEAL 촬영 표준화 가이드
+         *
+         * 검사 알고리즘이나 판정 기준은 변경하지 않고,
+         * 촬영 거리 / 각도 / 조명 / ROI 위치를
+         * 일정하게 맞출 수 있도록 안내합니다.
+         */
+        binding.btnSealCaptureGuide
+            .setOnClickListener {
+
+                CaptureGuideHelper.showGuideDialog(
+                    context = this,
+                    inspectionType = CaptureGuideHelper.TYPE_SEAL
+                )
+            }
+
+        /*
          * 뒤로가기
          */
         binding.btnSealBack
